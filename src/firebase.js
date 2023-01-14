f13188e4ae8d59aebf3470f99356f54393c852d1
Antonino Musmeci <nino.musmeci@gmail.com>
2023-01-14 14:47:16 +0100

    add .env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: process.env.REACT_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_FIREBASE_DB_URL,
+  projectId: process.env.REACT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_FIREBASE_BUCKET,
+  messagingSenderId: process.env.REACT_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_FIREBASE_MESUREMENT_ID,
 });
 
 export { firebaseConfig as firebase };
