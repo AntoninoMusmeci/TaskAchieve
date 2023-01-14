c6ff6e374cd105d39950fde30fc94d6791edb75b
Antonino Musmeci <nino.musmeci@gmail.com>
2023-01-14 15:28:56 +0100

    fix .env variable naming and firebase import

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,26 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/database";
+import "firebase/compat/storage";
 
-const firebaseConfig = firebase.initializeApp({
-  apiKey: process.env.REACT_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_FIREBASE_AUTHDOMAIN,
-  databaseURL: process.env.REACT_FIREBASE_DB_URL,
-  projectId: process.env.REACT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_FIREBASE_BUCKET,
-  messagingSenderId: process.env.REACT_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_FIREBASE_MESUREMENT_ID,
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MESUREMENT_ID,
 });
 
-export { firebaseConfig as firebase };
+export default firebase;
+
+/* ... */
+
+// // Initialize Firebase
+
+// firebase.initializeApp(firebaseConfig);
+
+// export default firebase; // re-export firebase library & namespace
