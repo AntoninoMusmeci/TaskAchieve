09c546291c1f0ae2b0ff01388eafae4d5440d6b5
Antonino Musmeci <nino.musmeci@gmail.com>
2023-01-18 21:59:08 +0100

    add ability to create a new task and fix data picker display logic

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { FaRegListAlt, FaRegCalendarAlt } from "react-icons/fa";
-import { useProjectsValue } from "../context";
+import { useProjectsValue, useTasksValue } from "../context";
 import moment from "moment/moment";
 import TaskDate from "./TaskDate";
+
 function AddTask({ showMainAddTask, setShowMainAddTask, projectName = "" }) {
   const [taskName, setTaskName] = useState("");
   const [showTaskDate, setShowTaskDate] = useState(false);
-  const [date, setDate] = useState();
-  // const {selectedProject} = useProjectsValue()
+  const [date, setDate] = useState("");
+  const { addTask } = useTasksValue();
+  const { selectedProject } = useProjectsValue();
+
   useEffect(() => {
     console.log("useEffectdate");
     setShowTaskDate(false);
@@ -25,7 +28,24 @@ function AddTask({ showMainAddTask, setShowMainAddTask, projectName = "" }) {
         </div>
         <div className="add-task__setting-buttons">
           <button onClick={() => setShowTaskDate(true)}>
-            {showTaskDate && <TaskDate setDate={setDate} date={date} />}{" "}
+            {showTaskDate && (
+              <div onClick={(e) => e.stopPropagation()}>
+                <div
+                  style={{
+                    cursor: "default",
+                    top: 0,
+                    left: 0,
+                    position: "fixed",
+                    width: "100%",
+                    height: "100%",
+                  }}
+                  onClick={() => {
+                    setShowTaskDate(false);
+                  }}
+                ></div>
+                <TaskDate setDate={setDate} date={date} />
+              </div>
+            )}{" "}
             <FaRegCalendarAlt />{" "}
             {date === moment().format("DD/MM/YYYY")
               ? "Today"
@@ -41,7 +61,12 @@ function AddTask({ showMainAddTask, setShowMainAddTask, projectName = "" }) {
       </div>
       <div className="add-task__add">
         <button onClick={() => setShowMainAddTask(false)}> Cancel </button>
-        <button disabled={taskName ? false : true}>Add Task</button>
+        <button
+          onClick={() => addTask(taskName, date, selectedProject)}
+          disabled={taskName ? false : true}
+        >
+          Add Task
+        </button>
       </div>
     </div>
   );
